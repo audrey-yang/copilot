ac64ebb484e05665373fe2a2812d12868ad7411c
Audrey Yang <29296576+audrey-yang@users.noreply.github.com>
2024-10-16 02:57:58 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,85 @@
+// Create web server
+// npm install express
+// npm install body-parser
+// npm install ejs
+// npm install mongoose
+// npm install method-override
+// npm install express-sanitizer
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    methodOverride = require("method-override"),
+    expressSanitizer = require("express-sanitizer");
+
+// App config
+mongoose.connect("mongodb://localhost:27017/restful_blog_app", {useNewUrlParser: true, useUnifiedTopology: true});
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
+
+// Mongoose model config
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+var Blog = mongoose.model("Blog", blogSchema);
+
+// RESTful routes
+app.get("/", function(req, res) {
+    res.redirect("/blogs");
+});
+
+// Index route
+app.get("/blogs", function(req, res) {
+    Blog.find({}, function(err, blogs) {
+        if(err) {
+            console.log("ERROR: " + err);
+        } else {
+            res.render("index", {blogs: blogs});
+        }
+    });
+});
+
+// New route
+app.get("/blogs/new", function(req, res) {
+    res.render("new");
+});
+
+// Create route
+app.post("/blogs", function(req, res) {
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.create(req.body.blog, function(err, newBlog) {
+        if(err) {
+            res.render("new");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+});
+
+// Show route
+app.get("/blogs/:id", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if(err) {
+            res.redirect("/blogs");
+        } else {
+            res.render("show", {blog: foundBlog});
+        }
+    });
+});
+
+// Edit route
+app.get("/blogs/:id/edit", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if(err) {
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+});
